c7d6493fad1b254bc65c36eafcd68736358f733a
uraniumCoder <williamzhaohaitong7@gmail.com>
2022-12-17 23:48:07 -0500

    small fix

--- src/data.py
+++ src/data.py
@@ -12,19 +12,19 @@ def read_form_b():
     # fix the column names to use hierarchical indexing
     experiments = ['cookies', 'cat', 'laundry', 'apples']
     form_b_questions = [
-        ('plan time', 1), ('plan time', 2), 
-        ('complete time', 1), ('complete time', 2),
-        ('plan prob', 1), ('plan prob', 2),
-        ('complete prob', 1), ('complete prob', 2),
-        ('partial difficulty', 'main'),
-        ('complete difficulty', 'main'),
+        ('plan', 'time', 1), ('plan', 'time', 2), 
+        ('complete', 'time', 1), ('complete', 'time', 2),
+        ('plan', 'prob', 1), ('plan', 'prob', 2),
+        ('complete', 'prob', 1), ('complete', 'prob', 2),
+        ('partial', 'difficulty', 'main'),
+        ('execute', 'difficulty', 'main'),
     ]
     form_b_expanded_toolset = [2, 1, 2, 1]
     form_b_columns = pd.MultiIndex.from_tuples(
-        [(e,t,
+        [(e,t,m,
             'main' if n in [form_b_expanded_toolset[i], 'main'] else 'other'
-        ) for (i,e),(t,n) in it.product(enumerate(experiments), form_b_questions)],
-        names=['experiment', 'question', 'toolset'])
+        ) for (i,e),(t,m,n) in it.product(enumerate(experiments), form_b_questions)],
+        names=['experiment', 'type', 'measurement', 'toolset'])
     form_b.columns = form_b_columns
     return form_b
 
